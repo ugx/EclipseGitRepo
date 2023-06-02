36ad041d7ee340c9835e39d6713ff03fcadaa4f7
usargin <usargin@WS105.es-software.local>
2023-06-02 13:28:14 +0200

    Comments & New Method

--- EclipseGitRepo/src/Start.java
+++ EclipseGitRepo/src/Start.java
@@ -1,8 +1,18 @@
-
+/**
+ * 
+ * @author ugx
+ * www.ugursargin.com
+ * 
+ */
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("Start..");
+		
+		System.out.println(getName("My Name"));
+	}
+	
+	private static String getName(String i_name) {
+		return i_name; 
 	}
 
 }
